61bd4263dc7d27fbb99dbdac27c95da9842ca834
Lutfiy <fiyanti33@gmail.com>
2020-01-07 15:33:17 +0700

    Perbaikan database dan buat model

---
+++ app/models/ncx.php
@@ -0,0 +1,41 @@
+<?php
+use Phalcon\Mvc\Model;
+class ncx extends Model
+{
+    public $id;
+	public $nama_cc;
+	public $nama_pekerjaan;
+	public $mitra;
+	public $nilai_nrc;
+	public $nilai_mrc;
+	public $status_ncx;
+	public $no_quote;
+	public $no_agreement;
+	public $tipe_order;
+	public $no_order_con; 
+	public $baso_con;
+	public $jenis_termin_con; 
+	public $billing_nol_con;
+	public $billcom_con; 
+	public $asset_con;
+	public $approval_sm_con; 
+	public $approval_ubc_con;
+	public $kendala_con;
+	public $dok_p6_cpe;
+	public $dok_p8_cpe;
+	public $dok_kl_wo_cpe;
+	public $dok_sm_crm_cpe;
+	public $no_order_cpe;
+	public $wfm_mitra_cpe;
+	public $approval_wfm_cpe;
+	public $status_nde_cpe;
+	public $approval_des_cpe;
+	public $baso_cpe;
+	public $jenis_termin_cpe;
+	public $billing_nol_cpe;
+	public $asset_cpe;
+	public $approval_sm_cpe;
+	public $approval_ubc_cpe;
+	public $billcom_cpe;
+	public $kendala_cpe;
+}
--- kptelkomncx.sql
+++ kptelkomncx.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 07 Jan 2020 pada 08.45
+-- Waktu pembuatan: 07 Jan 2020 pada 09.23
 -- Versi server: 10.1.36-MariaDB
 -- Versi PHP: 7.2.11
 
@@ -25,52 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `connectivity`
---
-
-CREATE TABLE `connectivity` (
-  `id` int(11) NOT NULL,
-  `no_order` varchar(225) DEFAULT NULL,
-  `baso` int(11) DEFAULT NULL,
-  `jenis_termin` int(11) DEFAULT NULL,
-  `billing_nol` date DEFAULT NULL,
-  `billcom` date DEFAULT NULL,
-  `asset` varchar(225) DEFAULT NULL,
-  `approval_sm` int(11) DEFAULT NULL,
-  `approval_ubc` int(11) DEFAULT NULL,
-  `kendala` varchar(1500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `cpe`
---
-
-CREATE TABLE `cpe` (
-  `id` int(11) NOT NULL,
-  `dok_p6` int(11) DEFAULT NULL,
-  `dok_p8` int(11) DEFAULT NULL,
-  `dok_kl_wo` int(11) DEFAULT NULL,
-  `dok_sm_crm` int(11) DEFAULT NULL,
-  `no_order` varchar(225) DEFAULT NULL,
-  `wfm_mitra` int(11) DEFAULT NULL,
-  `approval_wfm` int(11) DEFAULT NULL,
-  `status_nde` int(11) DEFAULT NULL,
-  `approval_des` int(11) DEFAULT NULL,
-  `baso` int(11) DEFAULT NULL,
-  `jenis_termin` int(11) DEFAULT NULL,
-  `billing_nol` date DEFAULT NULL,
-  `asset` varchar(225) DEFAULT NULL,
-  `approval_sm` int(11) DEFAULT NULL,
-  `approval_ubc` int(11) DEFAULT NULL,
-  `billcom` date DEFAULT NULL,
-  `kendala` varchar(1500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `ncx`
 --
 
@@ -85,8 +39,32 @@ CREATE TABLE `ncx` (
   `no_quote` varchar(225) DEFAULT NULL,
   `no_agreement` varchar(225) DEFAULT NULL,
   `tipe_order` int(11) DEFAULT NULL,
-  `id_connectivity` int(11) DEFAULT NULL,
-  `id_CPE` int(11) DEFAULT NULL
+  `no_order_con` varchar(225) DEFAULT NULL,
+  `baso_con` int(11) DEFAULT NULL,
+  `jenis_termin_con` int(11) DEFAULT NULL,
+  `billing_nol_con` date DEFAULT NULL,
+  `billcom_con` date DEFAULT NULL,
+  `asset_con` varchar(225) DEFAULT NULL,
+  `approval_sm_con` int(11) DEFAULT NULL,
+  `approval_ubc_con` int(11) DEFAULT NULL,
+  `kendala_con` varchar(1500) DEFAULT NULL,
+  `dok_p6_cpe` int(11) DEFAULT NULL,
+  `dok_p8_cpe` int(11) DEFAULT NULL,
+  `dok_kl_wo_cpe` int(11) DEFAULT NULL,
+  `dok_sm_crm_cpe` int(11) DEFAULT NULL,
+  `no_order_cpe` varchar(225) DEFAULT NULL,
+  `wfm_mitra_cpe` int(11) DEFAULT NULL,
+  `approval_wfm_cpe` int(11) DEFAULT NULL,
+  `status_nde_cpe` int(11) DEFAULT NULL,
+  `approval_des_cpe` int(11) DEFAULT NULL,
+  `baso_cpe` int(11) DEFAULT NULL,
+  `jenis_termin_cpe` int(11) DEFAULT NULL,
+  `billing_nol_cpe` date DEFAULT NULL,
+  `asset_cpe` varchar(225) DEFAULT NULL,
+  `approval_sm_cpe` int(11) DEFAULT NULL,
+  `approval_ubc_cpe` int(11) DEFAULT NULL,
+  `billcom_cpe` date DEFAULT NULL,
+  `kendala_cpe` varchar(1500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -94,57 +72,20 @@ CREATE TABLE `ncx` (
 --
 
 --
--- Indeks untuk tabel `connectivity`
---
-ALTER TABLE `connectivity`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `cpe`
---
-ALTER TABLE `cpe`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indeks untuk tabel `ncx`
 --
 ALTER TABLE `ncx`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_CPE` (`id_CPE`),
-  ADD KEY `id_connectivity` (`id_connectivity`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT untuk tabel `connectivity`
---
-ALTER TABLE `connectivity`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `cpe`
---
-ALTER TABLE `cpe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT untuk tabel `ncx`
 --
 ALTER TABLE `ncx`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `ncx`
---
-ALTER TABLE `ncx`
-  ADD CONSTRAINT `ncx_ibfk_1` FOREIGN KEY (`id_CPE`) REFERENCES `cpe` (`id`),
-  ADD CONSTRAINT `ncx_ibfk_2` FOREIGN KEY (`id_connectivity`) REFERENCES `connectivity` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

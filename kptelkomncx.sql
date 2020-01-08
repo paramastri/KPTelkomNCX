871a9e70ff82e6d05778dc8975cf7b42761f4e50
Lutfiy <fiyanti33@gmail.com>
2020-01-08 10:16:42 +0700

    database baru

--- kptelkomncx.sql
+++ kptelkomncx.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Jan 2020 pada 02.48
+-- Waktu pembuatan: 08 Jan 2020 pada 04.16
 -- Versi server: 10.1.36-MariaDB
 -- Versi PHP: 7.2.11
 
@@ -36,36 +36,10 @@ CREATE TABLE `ncx` (
   `nilai_nrc` varchar(225) DEFAULT NULL,
   `nilai_mrc` varchar(225) DEFAULT NULL,
   `status_ncx` varchar(225) DEFAULT NULL,
-  `kendala` text,
   `no_quote` varchar(225) DEFAULT NULL,
   `no_agreement` varchar(225) DEFAULT NULL,
-  `tipe_order` int(11) DEFAULT NULL,
-  `no_order_con` varchar(225) DEFAULT NULL,
-  `baso_con` int(11) DEFAULT NULL,
-  `jenis_termin_con` int(11) DEFAULT NULL,
-  `billing_nol_con` date DEFAULT NULL,
-  `billcom_con_nonter` date DEFAULT NULL,
-  `billcom_con_termin` date DEFAULT NULL,
-  `asset_con` varchar(225) DEFAULT NULL,
-  `approval_sm_con` int(11) DEFAULT NULL,
-  `approval_ubc_con` int(11) DEFAULT NULL,
-  `dok_p6_cpe` int(11) DEFAULT NULL,
-  `dok_p8_cpe` int(11) DEFAULT NULL,
-  `dok_kl_wo_cpe` int(11) DEFAULT NULL,
-  `dok_sm_crm_cpe` int(11) DEFAULT NULL,
-  `no_order_cpe` varchar(225) DEFAULT NULL,
-  `wfm_mitra_cpe` int(11) DEFAULT NULL,
-  `approval_wfm_cpe` int(11) DEFAULT NULL,
-  `status_nde_cpe` int(11) DEFAULT NULL,
-  `approval_des_cpe` int(11) DEFAULT NULL,
-  `baso_cpe` int(11) DEFAULT NULL,
-  `jenis_termin_cpe` int(11) DEFAULT NULL,
-  `billing_nol_cpe` date DEFAULT NULL,
-  `asset_cpe` varchar(225) DEFAULT NULL,
-  `approval_sm_cpe` int(11) DEFAULT NULL,
-  `approval_ubc_cpe` int(11) DEFAULT NULL,
-  `billcom_cpe_nonter` date DEFAULT NULL,
-  `billcom_cpe_termin` date NOT NULL
+  `kendala` text,
+  `tipe_order` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -77,16 +51,6 @@ CREATE TABLE `ncx` (
 --
 ALTER TABLE `ncx`
   ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `ncx`
---
-ALTER TABLE `ncx`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

e645866b3415dfc9cc344559caf610b0bd93a455
Lutfiy <fiyanti33@gmail.com>
2020-01-08 10:27:57 +0700

    model cpe dan connectivity

---
+++ app/models/connectivity.php
@@ -0,0 +1,14 @@
+<?php
+use Phalcon\Mvc\Model;
+class ncx extends Model{
+	public $id;
+	public $no_order_con; 
+	public $baso_con;
+	public $jenis_termin_con; 
+	public $billing_nol_con;
+	public $asset_con;
+	public $approval_sm_con; 
+	public $approval_ubc_con;
+	public $billcom_termin_con; 
+	public $billcom_nonter_con;
+}
---
+++ app/models/cpe.php
@@ -0,0 +1,23 @@
+<?php
+use Phalcon\Mvc\Model;
+class ncx extends Model{
+	public $id;
+	public $id_ncx;
+	public $dok_p6;
+	public $dok_p8;
+	public $dok_kl_wo;
+	public $dok_sm_crm;
+	public $no_order;
+	public $wfm_mitra;
+	public $approval_wfm;
+	public $status_nde;
+	public $approval_des;
+	public $baso;
+	public $jenis_termin;
+	public $billing_nol;
+	public $asset;
+	public $approval_sm;
+	public $approval_ubc;
+	public $billcom_nonter_cpe;
+	public $billcom_termin_cpe; 
+}
